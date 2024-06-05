49a862582e74bc4d0b5a4bbefe1c5ac649515b4b
Henri <henri.me@skiff.com>
2024-06-05 08:49:46 +0000

    web: added a hero section heading  and small description

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }): JSX.Element {
   return (
     <html lang="en">
-      <body className={`${GeistSans.className}`}>{children}</body>
+      <body className={GeistSans.className}>{children}</body>
     </html>
   );
 }
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -5,14 +5,10 @@ import { Button } from "@repo/ui/button";
 export default function Home() {
   return (
     <div>
-      <Card title="hey" href="wow">
-        <Code>
-          hey
-        </Code>
-      </Card>
-      <Button appName="wow">
-        hey
-      </Button>
+    <h2 className="text-3xl fon-medium">learn coding interactiv</h2>
+    <p>
+      With great articles, tutorials and in your speed
+    </p>
     </div>
   )
 }
